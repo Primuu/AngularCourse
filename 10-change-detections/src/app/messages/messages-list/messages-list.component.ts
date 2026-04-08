9b4d71e16af26ecc7552f1964c751c47ee90199e
Primuu <trentowski.adam@gmail.com>
2026-04-08 19:56:05 +0200

    OnPush strategy with singals

--- 10-change-detections/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detections/src/app/messages/messages-list/messages-list.component.ts
@@ -1,13 +1,16 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesListComponent {
-  messages = input.required<string[]>();
+  private msgService = inject(MessagesService);
+  messages = this.msgService.allMessages;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
