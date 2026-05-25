4d6c5e662b68a63ad905fb63e85fcf1b0acac9eb
Primuu <trentowski.adam@gmail.com>
2026-05-25 18:55:39 +0200

    Custom Observable

--- 11-rxjs-observables/src/app/app.component.ts
+++ 11-rxjs-observables/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {
   signal,
 } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -25,6 +25,21 @@ export class AppComponent implements OnInit {
 
   intervalSignal = toSignal(this.interval$, {
     initialValue: 0,
+  });
+
+  // ---------------------------------------
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...');
+      subscriber.next({ message: 'New value' });
+      timesExecuted++;
+    }, 2000);
   });
 
   constructor() {
@@ -52,6 +67,11 @@ export class AppComponent implements OnInit {
     // this.destroyRef.onDestroy(() => {
     //   subscription.unsubscribe();
     // });
+
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('Completed!'),
+    });
 
     const subscription = this.clickCount$.subscribe({
       next: (val) => console.log(`Clicked button ${val} times.`),
