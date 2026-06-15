f47362e1794843171884305190fc3fd7ec181600
Primuu <trentowski.adam@gmail.com>
2026-06-15 18:53:23 +0200

    error handling

--- 12-handling-responses/src/app/places/available-places/available-places.component.ts
+++ 12-handling-responses/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -22,6 +22,8 @@ export class AvailablePlacesComponent implements OnInit {
 
   isFetching = signal(false);
 
+  error = signal('');
+
   // constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
@@ -30,10 +32,21 @@ export class AvailablePlacesComponent implements OnInit {
       .get<{
         places: Place[];
       }>('http://localhost:3000/places') // , { observe: events, response }) etc.
-      .pipe(map((resData) => resData.places))
+      .pipe(
+        map((resData) => resData.places),
+        catchError((err, obs) => {
+          console.log(err);
+          return throwError(
+            () => new Error('Something went wrong while fetching...'),
+          );
+        }),
+      )
       .subscribe({
         next: (places) => {
           this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
         },
         complete: () => {
           this.isFetching.set(false);
