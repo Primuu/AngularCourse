738900c531ac1c1387d63c32ab15e26a6dd1d453
Primuu <trentowski.adam@gmail.com>
2026-06-16 17:44:29 +0200

    app-wide error handling

--- 12-handling-responses/src/app/places/places.service.ts
+++ 12-handling-responses/src/app/places/places.service.ts
@@ -3,12 +3,15 @@ import { inject, Injectable, signal } from '@angular/core';
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, tap, throwError } from 'rxjs';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
   private httpClient = inject(HttpClient);
+
+  private errorService = inject(ErrorService);
 
   private userPlaces = signal<Place[]>([]);
 
@@ -48,6 +51,7 @@ export class PlacesService {
       .pipe(
         catchError((err) => {
           this.userPlaces.set(prevPlaces);
+          this.errorService.showError('Failed to store selected place.');
           return throwError(() => new Error('Failed to store selected place.'));
         }),
       );
